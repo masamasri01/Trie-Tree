4976a565768b00af213aa79f10f203bd530cb86c
Masa Hassan Masri <93089580+masamasri01@users.noreply.github.com>
2022-12-16 11:07:03 +0200

    Update algorithm_hw2.cpp

--- algorithm_hw2/algorithm_hw2/algorithm_hw2.cpp
+++ algorithm_hw2/algorithm_hw2/algorithm_hw2.cpp
@@ -1,123 +1,115 @@
-﻿#include <vector>
+#include <vector>
 #include <iostream>
-
 #include <string>
 //// MASA HASSAN ADEL 11923986
 using namespace std;
 #define COUNT 10
 class Node {
 public:
-    char data;
+char data;
     vector <Node*> children;
     Node() {};
     Node(char data) { this->data = data; };
+  
+    
 
+     void InsertString(Node *root, char* s,int stringLength,int count) {
+         // count at first is arr length then start decreasing 
+       
+         if (count ==0) {
+           
+                    InsertChar(root, '$');
+                
+          //   printf("add $\n");
+             return;
+         }
+       
+         int idx = stringLength - count;
+       
+         count--;
+         bool flag = false;
 
+         if (root->children.empty())
+         {
+             InsertChar(root, s[idx]);
+             InsertString(root->children[0], s, stringLength, count);
+          }
 
-    void InsertString(Node* root, char* s, int stringLength, int count) {
-        // count at first is arr length then start decreasing 
-
-        if (count == 0) {
-
-
-            InsertChar(root, '$');
-
-        
-            //   printf("add $\n");
-            return;
-        }
-
-        int idx = stringLength - count;
-
-        count--;
-        bool flag = false;
-
-        if (root->children.empty())
-        {
-            InsertChar(root, s[idx]);
-            InsertString(root->children[0], s, stringLength, count);
-        }
-
-        else if (!root->children.empty()) {
-            for (size_t k = 0; k < root->children.size(); k++) {
-                if ((root->children[k]->data) == s[idx]) {
-                    flag = true;
-                    InsertString(root->children[k], s, stringLength, count);
-                    //dont insert it
+         else if (!root->children.empty() ) {
+              for (size_t k = 0; k < root->children.size(); k++) {
+                 if ((root->children[k]->data) == s[idx]) {
+                      flag = true;
+                     InsertString(root->children[k], s, stringLength, count);
+                   //dont insert it
                 }
-            }
-            if (flag == false) {
+               }
+             if (flag == false) {
                 // printf("hfft");
                //  printf("index is %d, data is %c parent is %c\n", idx, s[idx], root->data);
 
-                InsertChar(root, s[idx]);
-                InsertString(root->children[root->children.size() - 1], s, stringLength, count);
-            }
-        }
+                 InsertChar(root, s[idx]);
+                 InsertString(root->children[root->children.size()-1], s, stringLength, count);
+             }
+         }
 
-    }
+      }
 
-    //________________________________________________________________________
-    void InsertChar(Node* root, char c) {
-        root->children.push_back(new Node(c));
-        //   printf("reached\n");
+//________________________________________________________________________
+     void InsertChar(Node* root, char c) {
+         root->children.push_back(new Node(c));
+     //   printf("reached\n");
+         
+     }
+  
+    
+ 
+   
+     
 
-    }
-
-
-
-
-
-
-    bool searchFlag = false;
-    void  Search(Node* root, char* s, size_t arrLength, size_t idx) {
+   bool searchFlag = false;
+   void  Search(Node *root,char* s ,size_t arrLength,size_t idx) {
         // searchFlag = false;
-        searchFlag = false;
-        for (size_t i = 0; i < root->children.size(); i++) {
-
-            if (root->children[i]->data == s[idx]) {
-                if (idx == (arrLength - 1)) {
-
-                    for (size_t k = 0; k < root->children[i]->children.size(); k++) {
-                        if (root->children[i]->children[k]->data == '$') {
-
-                            searchFlag = true;
-                            return;
-                        }
-
+       searchFlag = false;
+       for (size_t i = 0; i < root->children.size(); i++) {
+           
+           if (root->children[i]->data == s[idx]){
+               if (idx == (arrLength - 1)) {
+                 
+                    for(size_t k=0; k< root->children[i]->children.size();k++){
+                    if(root->children[i]->children[k]->data == '$'){
+                       
+                       searchFlag = true;
+                       return;
+                    }
+                    
                     }
                 }
                 idx++;
                 Search(root->children[i], s, arrLength, idx);
+                  
+           } 
+         }
+       
 
-            }
-        }
-
-
-    }
-    /********           STARTTS WITH       *********/
-    void startsWith(Node* root, char* s, size_t arrLength, size_t idx) {
-        // if (idx == arrLength) return;
-        for (size_t i = 0; i < root->children.size(); i++) {
-            if (root->children[i]->data == s[idx]) {
-                if (idx == arrLength - 1) {
-                   //if(root->children[i]->data=='$')  cout << root->data << endl;
-                   // for(size_t b=0;b<)
-
-
-                    findStartWith(root->children[i], s);
+   }
+ /********           STARTTS WITH       *********/
+   void startsWith(Node *root, char *s, size_t arrLength, size_t idx) {
+      // if (idx == arrLength) return;
+       for (size_t i = 0; i < root->children.size(); i++) {
+           if (root->children[i]->data == s[idx]) {
+               if (idx == arrLength - 1) {
+                  
+                   findStartWith(root->children[i],s);
                     printStartWithInOrder();
-                    return;
-                }
-               
-                idx++;
-                startsWith(root->children[i], s, arrLength, idx);
-            }
-            
-        }
-
-    }
-    vector <string> starttt;
+                   return;
+               }
+               idx++;
+               startsWith(root->children[i], s, arrLength, idx);
+           }
+       }
+   
+   }
+ vector <string> starttt;
     void findStartWith(Node* root, string parentPath) {
 
         for (size_t k = 0; k < root->children.size(); k++) {
@@ -125,7 +117,6 @@ public:
            
             if ((root->children[k]->data == '$') ) {
                 currPath.pop_back();// remove $
-             //   cout << "currpath" << currPath << endl;
                 starttt.push_back(currPath);
                 
             }
@@ -134,24 +125,24 @@ public:
        
     }
     
-    void printStartWithInOrder() {
-      starttt= sort(starttt, starttt.size());
-       for (size_t k = 0; k < starttt.size(); k++) {
-           cout << starttt[k]<<endl;
-       }
-       starttt.clear();
+   void printStartWithInOrder() {
+        starttt = sort(starttt, starttt.size());
+        for (size_t k = 0; k < starttt.size(); k++) {
+            cout << starttt[k] << endl;
+        }
+        starttt.clear();
     }
     vector<string> sort(vector<string> s, size_t n)
     {
-        
-        for (int i = 1; i < n; i++)
+
+        for (size_t i = 1; i < n; i++)
         {
-            
+
             string temp = s[i];
 
             // Insert s[j] at its correct position
             int j = i - 1;
-            while (j >= 0 &&   isAlphaSmaller(temp,s[j]))
+            while (j >= 0 && isAlphaSmaller(temp, s[j]))
             {
                 s[j + 1] = s[j];
                 j--;
@@ -162,6 +153,8 @@ public:
     }
 
 
+
+    
     bool isAlphaSmaller(string s1, string s2) {
         size_t sizex = s1.length() > s2.length() ? s2.length() : s1.length();//smaller
         for (size_t i = 0; i < sizex; i++) {
@@ -169,29 +162,33 @@ public:
                 if (s1[i] == s2[i]) {
                     if (s1.length() < s2.length())
                         return true;
-                    else return false;
+                  //  else return false;
                 }
             }
-            if (s1[i] < s2[i]) return true;
-        }
+            
+            {
+             if (s1[i] < s2[i]) return true;
+              else if (s1[i] == s2[i]) continue;
+                else return false;
+        }}
         return false;
     }
-    /********    END       STARTTS WITH    END   *********/
+   /********    END       STARTTS WITH    END   *********/
 
 
-
- /**     LONGEST      **/
+ 
+/**     LONGEST      **/
 
     vector<char> Get_Max_Path(Node* root)
     {
         if (root->data == '$')  return vector <char>(0);
         vector<vector<char>> vec;
         for (size_t k = 0; k < root->children.size(); k++) {
-
+            
             vec.push_back(GetPath(root, Get_Max_Path(root->children[k])));
 
         }
-        return Max_Path(vec);
+        return Max_Path( vec);
     }
     vector<char> GetPath(Node* N, vector<char> v)
     {
@@ -201,7 +198,7 @@ public:
     vector<vector<char>> publicVec;
     vector<char> Max_Path(vector<vector<char>> vec)
     {
-
+        
         //publicVec = vec;
         size_t count = 0;
         vector<char> tmp;
@@ -217,18 +214,18 @@ public:
         return tmp;
     }
 
-    /**          END OF LONGEST   **/
+/**          END OF LONGEST   **/
 
-         /*     -----------------------------------------------    */
-         /*         STARTS DELETE          */
-    void deleteStringMain(Node* root, char* s, size_t length) {
-        Search(root, s, length, 0);
-        if (searchFlag == true) {
-            deleteStringFunc(root, s, length, 0);
-        }
-    }
+     /*     -----------------------------------------------    */
+     /*         STARTS DELETE          */
+  void deleteStringMain(Node* root ,char* s,size_t length) {
+      Search(root, s, length, 0);
+      if (searchFlag == true) {
+          deleteStringFunc(root, s, length,0);
+      }
+   }
 
-    void deleteChild(Node* root, char data) {
+  void deleteChild(Node* root, char data) {
         int counter = 0;
         Node* tmpNode;
         vector <Node*> tmp;
@@ -254,11 +251,11 @@ public:
         // printf("reached deleted %c\n", data);
 
     }
-    bool stop;
-    bool deleteStringFunc(Node* root, char* s, size_t length, size_t idx) {
-        stop = false;
-        for (size_t i = 0; i < root->children.size(); i++) {
-            if (root->children[i]->data == s[idx]) {
+  bool stop;
+  bool deleteStringFunc(Node* root, char* s, size_t length,size_t idx) {
+       stop = false;
+       for (size_t i=0;i< root->children.size();i++){
+             if (root->children[i]->data == s[idx]) {
                 if (deleteStringFunc(root->children[i], s, length, idx + 1)) {
                     if (stop == false) {
                         deleteChild(root, s[idx]);
@@ -270,54 +267,37 @@ public:
                         return false;
                     }
                 }
-            }
-            // cout << "kk" << root->children[i]->data << "idx==" << idx << endl;
-            if ((root->children[i]->data == '$') && (idx == length)) {
-                deleteChild(root, '$');
-                if (root->children.empty())
-                    return true;
-                else {
-                    stop = true;
-                    return false;
-                }
-            }
-
-        }
-        return false;
-    }
-    /*         END DELETE          */
-     /*     --------------------------------------------------------------    */
-    void print2D(Node* root)
-    {
-        if (root->data == '$') {
-            printf("\n");
-            cout << root->data << "\n";
-            return;
-        }
-        printf("\n");
-        cout << root->data << "\n";
-
-        for (size_t k = 0; k < root->children.size(); k++) {
-
-            print2D(root->children[k]);
-        }
-
-    }
-
-
+             }
+           // cout << "kk" << root->children[i]->data << "idx==" << idx << endl;
+           if ((root->children[i]->data == '$')&& (idx==length)) {
+              deleteChild(root, '$');
+              if (root->children.empty())
+                   return true;
+              else {
+                  stop = true;
+                  return false; }
+           }
+           
+          }
+          return stop;
+  }
+  /*         END DELETE          */
+   /*     --------------------------------------------------------------    */
+    
+  
 };
 
 vector<string> splitSpace(string cmd) {
     vector<string> out;
     string tmpWord = "";
-    for (size_t i = 0; i <= cmd.size(); i++) {
-        char x = cmd[i];
-
-        if ((x == ' ') || (i == cmd.size())) {
+    for (size_t i = 0; i <= cmd.size();i++) {
+       char x = cmd[i];
+      
+        if ((x == ' ')||(i== cmd.size())) {
             out.push_back(tmpWord);
-            tmpWord = "";
+          tmpWord = "";
         }
-
+            
         else
             tmpWord = tmpWord + x;
     }
@@ -328,51 +308,52 @@ int main() {
     /*   main root*/
     Node* root = new Node('*');
     /*   main root*/
+  
+    
+   string cmd;
+   vector<string> cmdWords;
+   string cmdWord;
+   string input;
 
-
-    string cmd;
-    vector<string> cmdWords;
-    string cmdWord;
-    string input;
-
-    // int iterationsCount;
-    // scanf("%d", &iterationsCount);
-    string iter;
-    getline(cin, iter);
-    for (int i = 0; i <stoi(iter); i++) {
-        getline(cin, cmd);
+  // int iterationsCount;
+  // scanf("%d", &iterationsCount);
+   string iter;
+   getline(cin, iter);
+   for (int i=0;i< stoi(iter); i++) {
+        getline(cin, cmd);  
         if (cmd == "longest") {
-            vector<char> vecoflongest = root->Get_Max_Path(root);
-            size_t vecSize = vecoflongest.size();
-            for (size_t m = 1; m < vecSize; m++) printf("%c", vecoflongest[vecSize - m - 1]);
-            printf("\n");
+             vector<char> vecoflongest = root->Get_Max_Path(root);
+             size_t vecSize = vecoflongest.size();
+             for (size_t m = 1; m < vecSize; m++) printf("%c", vecoflongest[vecSize - m - 1]);
+             printf("\n");
         }
 
-        else {
-            cmdWords = splitSpace(cmd);
-            cmdWord = cmdWords[0];
-            input = cmdWords[1];
-            char* arr = &input[0];
+        else{
+        cmdWords = splitSpace(cmd);
+        cmdWord = cmdWords[0];
+        input = cmdWords[1];
+        char* arr = &input[0];
 
-            if (cmdWord == "insert") {
-                root->InsertString(root, arr, (int)input.length(), (int)input.length());
-            }
-            else if (cmdWord == "search") {
-                root->Search(root, arr, (int)input.length(), 0);
+          if (cmdWord == "insert") {
+              root->InsertString(root, arr, (int)input.length(), (int)input.length());
+          }
+          else if (cmdWord == "search") {
+              root->Search(root, arr, (int)input.length(), 0);
 
-                if (root->searchFlag == true) printf("YES\n"); else  printf("NO\n");
-
-            }
-            else if (cmdWord == "delete") {
-                root->Search(root, arr, input.length(), 0);
-                root->deleteStringMain(root, arr, input.length());
-            }
-            else if (cmdWord == "startwith") {
-                root->startsWith(root, arr, input.length(), 0);
-            }
+              if (root->searchFlag == true) printf("YES\n"); else  printf("NO\n");
+             
+          }
+          else if (cmdWord == "delete") {
+               root->Search(root, arr, input.length(), 0);
+               root->deleteStringMain(root,arr, input.length());
+          }
+          else if (cmdWord == "startwith") {
+              root->startsWith(root, arr, input.length(), 0);
+          }
         }
-      //  root->print2D(root);
-    }
-
-
+   }
+  
+   
 };
+    
+                           
